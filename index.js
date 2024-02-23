b21f854d5d71ed90391c1c2c9aae8e7c481b3e70
Tim Susa Work <timsusawork@0bf7233e-0374-4eaa-85f9-bbb912c6262c.fritz.box>
2024-02-23 12:17:56 +0100

    Feat: add loading of user resume

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 import axios from "axios";
 import { getToken } from "./auth.js";
 
-// https://api.cinode.com/v0.1/companies/456/absence/types
-async function getCompanyCandidates() {
+const headers = {
+    'Authorization': `Bearer ${(await getToken()).access_token}`,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+export async function getUsers() {
     try {
-        const { data } = await axios.get(`${process.env.API_ENDPOINT}/companies/${process.env.COMPANY_ID}`,
+        const { data } = await axios.get(`${process.env.API_ENDPOINT}/companies/${process.env.COMPANY_ID}/users`,
             {
-                headers: {
-                    'Authorization': `Bearer ${(await getToken()).access_token}`,
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
+                headers
             });
-        console.log('getCompanyCandidates ', data);
+        console.log('getUsers ', data);
     } catch (error) {
         throw new Error('Error getting company candidates ', error)
     }
 }
-await getCompanyCandidates();
+
+
+
+export async function getResumes() {
+    try {
+        const { data } = await axios.get(`${process.env.API_ENDPOINT}/companies/${process.env.COMPANY_ID}/resumes`,
+            {
+                headers
+            });
+        console.log('getResumes ', JSON.stringify(data, null, 2));
+    } catch (error) {
+        throw new Error('Error getting company candidates ', error)
+    }
+}
+
+
+
+export async function getUserResume(userId = 161192, resumeId = 276675) {
+    try {
+        const { data } = await axios.get(`${process.env.API_ENDPOINT}/companies/${process.env.COMPANY_ID}/users/${userId}/resumes/${resumeId}`,
+            {
+                headers
+            });
+        console.log('getUserResume ', JSON.stringify(data, null, 2));
+    } catch (error) {
+        throw new Error('Error getting company candidates ', error)
+    }
+}
+
+
+await getUserResume();
